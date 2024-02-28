570bddf87b65bc4bf92170bc00ba888deda09dc8
yokhor <yokhor06@mail.ru>
2024-02-28 19:35:25 +1000

    rewritten parses + other changes

--- fetch_data.py
+++ fetch_data.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -15,68 +17,70 @@ def collect_data(date: str):
 
 
 # delete day of the week and extra whitespaces
-def get_clear_text(text: str):
+def get_clear_text(text: str) -> tuple:
     text = text.strip()
     days_of_week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+    found_day = None
     for day in days_of_week:
         rubbish = str(day + ', ')
         if rubbish in text:
             text = text.removeprefix(rubbish)
+            found_day = day
             break
-    return text
-        
-        
-# write the event; if success, return true
-def write_event(curr_date, curr_time, curr_name, theatres, events):
-    if curr_date and curr_time and curr_name:  # protection from first lines rubbish
-        for theatre in theatres:  # several theatres for the same film at the same time
-            events[curr_date][curr_time].append((curr_name, theatre))
-        return True
-    return False
+    return text, found_day
+
+
+# list of pairs film_name - theatre (possibly more than one theatre)
+def name_to_theatre(elem) -> list:
+    result = []
+    film_name = elem.get_text().strip()
+    for theatre in elem.parent.find(class_='table-responsive__theatre-name').find_all('a'):
+        result.append((film_name, theatre.get_text()))
+    return result
+
+
+# write the event to the dict
+def write_event(curr_date: str, curr_time: str, events: dict, pairs: list) -> None:
+    for [name, theatre] in pairs:
+        events[curr_date][curr_time].append((name, theatre))
 
 
 # parse films from the HTML document
-def parse_data(date: str):
+def parse_data(date: str) -> tuple:
     # `date` means the real today's date; `curr_date` and `curr_time` are intended for currently parsing elements
     with open(f'data_{date}.html') as file:
         soup = BeautifulSoup(file, 'html.parser')
 
     events = {}
+    dates_days_of_week = {}
     curr_date = ""
-    curr_time = ""
-    curr_name = ""
-    curr_theatres = []
-    for tr in soup.find_all('tr'):
-        tds = tr.contents
-        for td in tds:
-            try:
-                if td.contents[1]['class'][0] == 'films-seances__seance-date':  # found new date mark
-                    if write_event(curr_date, curr_time, curr_name, curr_theatres, events):
-                        curr_theatres = []
-                    curr_date = get_clear_text(td.contents[1].get_text()[1:])
-                    events[curr_date] = {}
-                    curr_time = ""
-            except:
-                try:
-                    if curr_time and td['class'][0] == 'table-responsive__theatre-name':  # found a new theatre name
-                        theatres = td.get_text()[1:-1]
-                        tmp = theatres.split('\n')  # one film at the same time can have several theatres
-                        for e in tmp:
-                            curr_theatres.append(e)
-                    else:
-                        if write_event(curr_date, curr_time, curr_name, curr_theatres, events):
-                            curr_theatres = []
-                        if td['class'][0] == 'time':  # found a new time mark (they have one tag for several films)
-                            curr_time = get_clear_text(td.get_text())
-                            events[curr_date][curr_time] = []
-                        elif curr_time and td['class'][0] == 'table-responsive__film-name':  # found a new film name
-                            curr_name = td.get_text()[1:-1]
-                except:
-                    continue
-    return events
+
+    trs = soup.find_all('tr')
+    i = 0
+    while i < len(trs):  # while-loop is chosen to be able to skip some elements from the inside
+        tr = trs[i]
+        for_date = tr.find(class_="films-seances__seance-date")
+        for_time = tr.find(class_='time')
+        if for_date:  # new date mark
+            curr_date, day_of_week = get_clear_text(for_date.get_text())
+            dates_days_of_week[curr_date] = day_of_week
+            events[curr_date] = {}
+        elif for_time:  # new time mark (they have one time tag for several films)
+            curr_time, _ = get_clear_text(for_time.get_text())
+            events[curr_date][curr_time] = []
+            for_name = trs[i].find(class_='table-responsive__film-name')
+            write_event(curr_date, curr_time, events, name_to_theatre(for_name))  # the same elem contains film name
+            if 'rowspan' in for_time.attrs:
+                for j in range(int(for_time['rowspan']) - 1):  # the next `rowspan` elems contain films for `curr_time`
+                    i += 1
+                    for_name = trs[i].find(class_='table-responsive__film-name')
+                    write_event(curr_date, curr_time, events, name_to_theatre(for_name))
+        i += 1
+
+    return events, dates_days_of_week
 
 
-def save_data(events: dict, date: str):
+def save_data(events: dict, days_of_week: dict, date: str) -> None:
     # create json
     with open(f'data_{date}.json', 'w') as file:
         json.dump(events, file, indent=4, ensure_ascii=False)
@@ -85,25 +89,29 @@ def save_data(events: dict, date: str):
     with open(f'data_{date}.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerow(
-            ('Date', 'Time', 'Film', 'Theatre')
+            ('Дата', 'День недели', 'Время', 'Фильм', 'Кинотеатр')
         )
 
     # write contents
     with open(f'data_{date}.csv', 'a') as file:
         writer = csv.writer(file)
         for [f_date, times] in events.items():
+            day_week = days_of_week[f_date]
             for [time, films] in times.items():
                 for [name, theatre] in films:
                     writer.writerow(
-                        (f_date, time, name, theatre)
+                        (f_date, day_week, time, name, theatre)
                     )
-    
+
+    os.system(f'libreoffice --convert-to ods data_{date}.csv')  # convert to .ods
+    os.system(f'libreoffice --convert-to xlsx data_{date}.csv')  # and .xlsx
+
 
 def main():
     t_date = datetime.now().strftime('%d_%m_%Y')
     collect_data(t_date)
-    events = parse_data(t_date)
-    save_data(events, t_date)
+    events, days_of_week = parse_data(t_date)
+    save_data(events, days_of_week, t_date)
     
 
 def fetch_data():
---
+++ info.md
@@ -0,0 +1,3 @@
+Разработчик: @yokhor
+Версия: `1.1`
+GitHub: https://github\.com/yokhor/cinema\_vladivostok
